9da26bd96316a26dc0560bba786b418a44db8941
filip.lezuch <filiplez18@interia.pl>
2019-08-30 13:20:29 +0200

    initial commit

--- miner.py
+++ miner.py
@@ -1,1 +1,8 @@
+import hashlib
+from time import sleep
 
+print(hashlib.algorithms_available)
+print(hashlib.algorithms_guaranteed)
+
+
+wall = 'Let me point out that normally you would not include any sort of formatting in a PowerShell function, nor would you use Write-Host to display results. But in this situation, that’s exactly what I want. The only purpose of this function is to show information on the screen.'
---
+++ transaction.py
@@ -0,0 +1,24 @@
+import datetime
+import collections
+
+
+class Transaction:
+    def __init__(self, sender, recipient, value):
+        self.sender = sender
+        self.recipient = recipient
+        self.value = value
+        self.time = datetime.datetime.now()
+
+    def merge_to_dict(self):
+        if self.sender == "Genesis":
+            identity = "Genesis"
+        else:
+            identity = self.sender.identity
+
+        return collections.OrderedDict({
+            'sender': identity,
+            'recipient': self.recipient,
+            'value': self.value,
+            'time': self.time})
+
+
